51d69ba4f22a68adcb259bf1e3cf5d8bdbed7fe0
EHTASHAM TOOR <ehtashamtoor50@gmail.com>
2023-03-03 10:54:17 +0500

    Signup and login validation

---
+++ src/Components/Main Website/Schemas/index.jsx
@@ -0,0 +1,18 @@
+import * as Yup from "yup"
+
+export const RegisterSchema = Yup.object(
+    {
+        name: Yup.string().required("Username is required").min(2, "Username should be at least 2 characters").max(25, "Name should be less than 26 characters"),
+        email: Yup.string().email().required("Please enter your email").matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z]{2,}$/i, 'email must be a valid email'),
+        password: Yup.string().required("Please enter your password").min(6, "Password must be more than 6 characters"),
+        confirmPassword: Yup.string().required("confirm password is required").min(6).oneOf([Yup.ref("password")], "Passwords must match"),
+    }
+)
+
+export const LoginSchema = Yup.object(
+    {
+        name: Yup.string().required("Username is required").min(2, "Username should be at least 2 characters"),
+        password: Yup.string().required("Please enter your password").min(6, "Password must be more than 6 characters"),
+    }
+)
+
