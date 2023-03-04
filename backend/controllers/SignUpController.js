221c34f09cc0ea450ce8394eb48999084344cbec
EHTASHAM TOOR <ehtashamtoor50@gmail.com>
2023-03-04 11:55:55 +0500

    userschema made userschema.js

--- backend/controllers/SignUpController.js
+++ backend/controllers/SignUpController.js
@@ -1,10 +1,29 @@
+/* eslint-disable no-unused-vars */
+const User = require("../Models/UserSchema")
 
-const SignUpData = (req, res) => {
-    // console.log("route ran")
-    const userData = req.body.data;
+const SignUpData = async (req, res) => {
+    const { name, email, password } = req.body.data;
 
+    try {
+        const UserExist = await User.findOne({ email: email });
 
-    console.log(userData)
+        if (UserExist) {
+            return res.status(422).json({ error: "Email already exists" });
+        }
+
+        const user = new User({ name, email, password });
+
+        const userRegister = await user.save()
+
+        if (userRegister) {
+            return res.status(201).json({ mesage: "User Registered Successfully" })
+        } else {
+            return res.status(500).json({ mesage: "Registration Failed" })
+        }
+
+    } catch (error) {
+        console.loG(error.message);
+    }
 }
 
 module.exports = {
