c150f25d750abbddaed9c4bc5a3d1c671a8d2053
Muhammad Hamad khan <muhammadhamadkhan023@gmail.com>
2023-03-09 20:35:17 +0500

    changed

---
+++ server/models/testimonials.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+let testimonialsSchema = mongoose.Schema({
+  
+     name:{
+        type:String,
+        required:true
+    },
+    picture:{
+        type:String,
+        required:true
+    },
+    review:{
+        type:String,
+        required:true
+    },
+   rating:{
+        type:String,
+        required:true
+    },
+    
+
+
+})
+
+module.exports=mongoose.model('testimonials',testimonialsSchema);
