42b4ba84f1903f5938a4c4492862aefb3c9342f1
Muhammad Hamad khan <muhammadhamadkhan023@gmail.com>
2023-03-09 15:33:46 +0500

    changes

--- server/controllers/addCourseController.js
+++ server/controllers/addCourseController.js
@@ -2,43 +2,22 @@ let express = require('express');
 let multer =require('multer');
 let router = express.Router();
 let Course = require('../models/course');
-let path = require('path');
-const fs = require('fs');
-
-// write a code to store images in server through multer and create folder through fs and return link to store in mongoDB?
 
 
-const upload = multer({ dest: 'server/uploads/media' })
-  
-//   const upload = multer({ storage: storage })
 
-// app.post('/', upload.single('image'), (req, res) => {
-//   res.send(`/${req.file.path}`)
-// })
-
-router.post('/addCourse', upload.single('courseCardPic'), async (req, res) => {
+router.post('/addCourse',  async (req, res) => {
 
     try {
         console.log(req.body);
-        const file = req.file;
-        // Save the file to disk using fs
-        const filename = `${file.originalname}`;
-        const filepath = `server/upload/media/${filename}`;
-        // fs.writeFile(filename, filepath, 'base64', (err) => {
-        //     if (err) throw err;
-        //     console.log('Image file saved successfully.');
-        //   });
-        // fs.writeFile(filepath)      
-        // Generate a URL for the file
-        const url = filepath;
-      
+        
+     
         let course = new Course(req.body);
-        course.courseCardPic =  url;
+    
+
         await course.save();
         res.send({ message: "Course Added Successfully" });
-        
     }
-
+    
     catch (error) {
         res.send({ message: "internal Server Error" });
         console.log(error.message)
@@ -47,8 +26,8 @@ router.post('/addCourse', upload.single('courseCardPic'), async (req, res) => {
 
 router.get('/',async(req,res)=>{
     try {     
-        let applications = await CourseApply.find();        
-        res.json(applications);
+        let Courses = await Course.find();        
+        res.json(Courses);
     } catch (e) {
         console.error(e.message);
         res.status(500).json('Server Error');
--- src/Components/Main Website/page/course.jsx
+++ src/Components/Main Website/page/course.jsx
@@ -11,10 +11,11 @@ import SEO from '../assets/images/CoursePics/SEO.jpeg';
 import SMM from '../assets/images/CoursePics/SMM.jpeg';
 import GameDevelop from '../assets/images/CoursePics/GameDevelopment.jpeg';
 import VideoEditing from '../assets/images/CoursePics/Video Editing.jpeg';
-// import testing from './server/uploads/media/f4d4038eb10137262628d31fe6ce7df6-changing-from-web-2-0-to-web-3-0-concept-2022-12-16-11-14-37-utc.jpg'
 import ReactPaginate from 'react-paginate';
-
-const courseList = [
+import { useEffect  } from "react";
+import axios from "axios";
+// import { } from "react";
+const course = [
     {
         imgUrl: DigitalPic,
         imgAlt: 'Digital Marketing Course',
@@ -66,7 +67,7 @@ const courseList = [
         title: 'Unity 3D Game Development',
         totalLeson: '3 Months',
         schdule: 'Physical Class',
-        authorImgUrl: 'server/upload/media/5662d0c8e64f6d9ede84861f9d6a8c7a-changing-from-web-2-0-to-web-3-0-concept-2022-12-16-11-14-37-utc.jpg',
+        authorImgUrl: 'http://localhost:5000/server/uploads/changing-from-web-2-0-to-web-3-0-concept-2022-12-16-11-14-37-utc.jpg',
         authorImgAlt: 'course author rajibraj91 rajibraj',
         authorName: 'Zinat Zaara',
         btnText: 'Read More',
@@ -141,8 +142,9 @@ const courseList = [
     },
 ]
 
-
 const CurrentItems = ({ Items }) => {
+ 
+    
     return (<>
         {
             Items && Items.map((val, i) => {
@@ -150,7 +152,7 @@ const CurrentItems = ({ Items }) => {
                     <div className="course-item">
                         <div className="course-inner">
                             <div className="course-thumb">
-                                <img src={`${val.imgUrl}`} alt={`${val.imgAlt}`} />
+                                <img src={`${val.courseCardPic}`} alt={`${val.imgAlt}`} />
                             </div>
                             <div className="course-content">
                                 <div className="course-price" style={{ backgroundColor: "#ff0911" }}>{val.price}</div>
@@ -163,14 +165,14 @@ const CurrentItems = ({ Items }) => {
                                         <span className="ratting-count"> {val.reviewCount}</span>
                                     </div>
                                 </div>
-                                <Link to="/course-single"><h4>{val.title}</h4></Link>
+                                <Link to="/course-single"><h4>{val.courseTitle}</h4></Link>
                                 <div className="course-details">
-                                    <div className="couse-count"><i className="icofont-video-alt"></i> {val.totalLeson}</div>
-                                    <div className="couse-topic"><i className="icofont-signal"></i> {val.schdule}</div>
+                                    <div className="couse-count"><i className="icofont-video-alt"></i> {val.courseLessons}</div>
+                                    <div className="couse-topic"><i className="icofont-signal"></i> {val.courseDuration}</div>
                                 </div>
                                 <div className="course-footer">
                                     <div className="course-author">
-                                        <img src={`${val.authorImgUrl}`} alt={`${val.authorImgAlt}`} />
+                                        <img src={`${val.courseCardPic}`} alt={`${val.authorImgAlt}`} />
                                         <Link to="/team-single" className="ca-name">{val.authorName}</Link>
                                     </div>
                                     <div className="course-btn">
@@ -189,22 +191,36 @@ const CurrentItems = ({ Items }) => {
 
 const CoursePage = ({ itemsPerPage }) => {
 
+    let [course , setCourse] = useState([])
+
+    useEffect( ()=>{
+     
+        async function fetchCourse() { 
+          let resp = await axios.get('/course/');
+          console.log(resp.data);
+          setCourse(resp.data);
+        }
+        fetchCourse();  
+        
+        }, []);
+    
+
     const [startItems, setstartItems] = useState(0);
 
     let endItems = startItems + itemsPerPage;
 
-    if (endItems > courseList.length) {
-        endItems = courseList.length;
+    if (endItems > course.length) {
+        endItems = course.length;
     }
 
-    // Getting items from the CourseList array to display current items
-    const currentItems = courseList.slice(startItems, endItems);
+    // Getting items from the course array to display current items
+    const currentItems = course.slice(startItems, endItems);
     console.log(currentItems)
-    const pageCount = Math.ceil(courseList.length / itemsPerPage);
+    const pageCount = Math.ceil(course.length / itemsPerPage);
 
     // Function to handle Page click
     const handlePageClick = (event) => {
-        const newstartItem = (event.selected * itemsPerPage) % courseList.length;
+        const newstartItem = (event.selected * itemsPerPage) % course.length;
 
         setstartItems(newstartItem);
     };
